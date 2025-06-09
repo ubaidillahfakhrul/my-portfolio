f08cd61b460de776a98dc42820d32596b00004e6
ubaidillah <ubaidillah04@yahoo.com>
2025-06-09 08:39:03 +0700

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 // import React from 'react';
-import React, { useMemo } from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import useDarkMode from 'use-dark-mode';
-import AppContext from './AppContext';
-import MainApp from './MainApp';
-import GlobalStyles from './theme/GlobalStyles';
-import { lightTheme, darkTheme } from './theme/themes';
+import React, { useMemo } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import useDarkMode from "use-dark-mode";
+import AppContext from "./AppContext";
+import MainApp from "./MainApp";
+import GlobalStyles from "./theme/GlobalStyles";
+import { lightTheme, darkTheme } from "./theme/themes";
 
 function App() {
   window.matchMedia = null;
@@ -21,7 +21,8 @@ function App() {
         <GlobalStyles />
         <div className="App">
           {/* ini kalau untuk localhost "basename/my-portfolio" di hapus */}
-          <BrowserRouter basename={process.env.NODE_ENV === "production" ? "/my-portfolio" : "/"}>   
+          {/* <BrowserRouter basename={process.env.NODE_ENV === "production" ? "/my-portfolio" : "/"}>    */}
+          <BrowserRouter basename="my-portfolio">
             <MainApp />
           </BrowserRouter>
         </div>
